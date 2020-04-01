0cafcd7cf989cdd5801cada1b73ee01097c64226
Surbhi Jain <surbhi.jain@nagarro.com>
2020-04-02 02:38:53 +0530

    event binding added

--- angular-basics-app/src/app/users/user-list.component.ts
+++ angular-basics-app/src/app/users/user-list.component.ts
@@ -7,20 +7,25 @@ import { Component } from '@angular/core';
 export class UserListComponent {
     pageTitle: string = "User List";
     filterInputPlaceholder: string = "Enter name here";
-    users: any[] = [
-        {
-            "name": "Neha Jain",
-            "email": "nj@gmail.com",
-            "phoneNumber": "9876543210",
-            "username": "nehaJain",
-            "age": 26
-        },
-        {
-            "name": "Aastha Jain",
-            "email": "aj@gmail.com",
-            "phoneNumber": "8876543210",
-            "username": "aasthaJain",
-            "age": 26
+    users: any[] = [{
+        "id": 1,
+        "name": "Neha Jain",
+        "email": "nj@gmail.com",
+        "phoneNumber": "9876543210",
+        "username": "nehaJain",
+        "age": 26
+    }, {
+        "id": 2,
+        "name": "Aastha Jain",
+        "email": "aj@gmail.com",
+        "phoneNumber": "8876543210",
+        "username": "aasthaJain",
+        "age": 26
+    }];
+    deleteRecord(id: number): void {
+        let index: number = this.users.findIndex(user => user.id == id);
+        if (index != -1) {
+            this.users.splice(index, 1)
         }
-    ]
+    }
 }
